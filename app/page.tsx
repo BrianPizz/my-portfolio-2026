6fc414fea7f889588fb1f033146659797d9a73fc
BrianPizz <bpizzimentijr@outlook.com>
2025-10-04 15:55:20 -0400

    add dither background and title overlay

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import Dither from "@/components/Dither"
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
-        <h1>
-          Hello World
-        </h1>
+    <div className="fixed inset-0 w-full h-full">
+      <div className="absolute inset-0 z-0 w-full h-full">
+        <Dither />
       </div>
-    </main>
+      <div className="relative z-10 w-full h-full flex items-center justify-center">
+        <Hero />
+      </div>
+    </div>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36 relative z-10'>
+        <div className='text-center'>
+            <h1 className='text-6xl md:text-8xl font-bold text-white mb-6'>
+                Brian Pizzimenti
+            </h1>
+            <p className='text-xl md:text-2xl text-gray-300 max-w-2xl mx-auto'>
+                Full Stack Developer
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
